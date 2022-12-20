b54e481c77028743a9f9bb3c43184e808bba4857
Rubi Vargas <rubie9816@gmail.com>
2022-12-19 16:10:51 -0800

    modify groovy

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -9,21 +9,35 @@ def call(Map params){
         stages {
             stage('Github stage') {
                 steps {
-                   
+                    // git(
+                    //     url: 'https://github.com/sparktechsllc/DISC.git',
+                    //     credentialsId: 'jonathan-github-token',
+                    //     branch: params.GitBranch
+                    // )
                 }
             }
             
             stage('Build Stage') {
                 steps {
-                  echo ${params.main}
-                    }
+                    // script {
+                        echo "[+] ---> Building ${params.main}"
+                    //     docker_image = docker.build(
+                    //         "${params.Repo}:${params.Tag}",
+                    //         "-f ${params.Dockerfile} ${params.DockerfileContext}"
+                    //     )
+                    // }
                 }
             }
             
             stage('Push Stage') {
                 steps {
-                   
-                    }
+                    // script{
+                    //     docker.withRegistry("https://${params.Repo}", params.RegistryCredentials) {
+                    //         echo "[+] ---> Pushing to https://${params.Repo}"
+                    //         docker_image.push('latest')
+                    //         docker_image.push()
+                    //     }
+                    // }
                 }
             }
         }
@@ -31,8 +45,9 @@ def call(Map params){
 		
 		post{
 			success {
-				script {
-				}
+				// script {
+				// 	dockerDefs.pruneImages()
+				// }
 			}
 		}
 	}
